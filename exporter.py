51a9a8190455dd7bc189bfd350a93a89137139b1
ToWipf <tobf@web.de>
2023-03-05 11:12:23 +0100

    add on/offline info

--- exporter.py
+++ exporter.py
@@ -3,8 +3,9 @@ import requests
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 def doScan(url, user, password):
+    prom_output = "# Solar Exporter\n"
     try:
-        data = requests.get(url, auth=(user,password))
+        data = requests.get("http://{}/status.html".format(url), auth=(user,password))
 
         for zeile in data.text.split('\n'):
 
@@ -13,18 +14,16 @@ def doScan(url, user, password):
                 left_texter = watt_now.find('"', 0, len(watt_now)) + 1
                 right_texter = watt_now.find('"', left_texter, len(watt_now) )
                 watt_now=watt_now[left_texter:right_texter]
-                return watt_now
+                prom_output += "watt {}\nonline 1".format(watt_now)
     except Exception:
-        return "0"
+        prom_output += "watt 0\nonline 0"
+    return prom_output
 
 class StartScan(BaseHTTPRequestHandler):
     def do_GET(self):
-        url = "http://{}/status.html".format("192.168.2.15")
-        message = "watt {}".format(doScan(url, 'admin', 'admin'))
-
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(message.encode())
+        self.wfile.write(doScan("192.168.2.15", 'admin', 'admin').encode())
         return
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -1,8 +1,17 @@
 # sun600g3-eu-230 exporter Prometheus
 
 ## Build:
+
 ```sh
 DOCKER_BUILDKIT=1 docker build -t x .
 
 docker run -d --name solar_exporter -p 9942:9942 solar_exporter
+```
+
+## Example output
+
+```yml
+# Solar Exporter
+watt 6
+online 1
 ```
